c8d8d2681fded124c76b2c7db85341de2ba21c26
WangKaixuan <2654189525@qq.com>
2019-09-04 14:34:48 +0800

    reorganize code

--- src/rlp.c
+++ src/rlp.c
@@ -312,12 +312,3 @@ void test_hex2dec()
     assert(len == 4090752290);
 }
 
-int main(int argc, char const *argv[])
-{
-    test_hex2dec();
-    test_get_decode_length();
-    test_rlp_decode();
-    test_rlp_decode1();
-    test_rlp_decode2();
-    return 0;
-}
