5f4dc5f5c28d15ac859a1b729315c2f3abd20fba
wangkaixuan <2654189525@qq.com>
2019-09-04 13:09:09 +0800

    add test_get_decode_length

--- rlp.c
+++ rlp.c
@@ -4,6 +4,7 @@
 #include <math.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <assert.h>
 
 #define DECODE_RESULT_LEN 100
 
@@ -21,7 +22,7 @@ typedef struct
     uint8_t used_index;
 } decode_result;
 
-int get_decode_length(uint8_t *seq, int seq_len, uint8_t *decoded_len, seq_type *type)
+int get_decode_length(uint8_t *seq, int seq_len, int *decoded_len, seq_type *type)
 {
     uint8_t first_byte = *seq;
     int item_bytes_len = 0;
@@ -63,15 +64,52 @@ int get_decode_length(uint8_t *seq, int seq_len, uint8_t *decoded_len, seq_type 
     return item_bytes_len;
 }
 
+void test_get_decode_length()
+{
+    uint8_t buf;
+    int decoded_len = 0;
+    seq_type type = NONE;
+    int read_len=0;
+
+    hex_to_buffer("00", 2, &buf, 1);
+    read_len = get_decode_length(&buf, 1, &decoded_len, &type);
+    assert(decoded_len == 0 && type == STRING && read_len == 1);
+
+    hex_to_buffer("88", 2, &buf, 1);
+    read_len = get_decode_length(&buf, 9, &decoded_len, &type); // 假设长度大于8
+    assert(decoded_len == 1 && type == STRING && read_len == 8);
+
+    hex_to_buffer("c9", 2, &buf, 1);
+    read_len = get_decode_length(&buf, 10, &decoded_len, &type);
+    assert(decoded_len == 1 && type == LIST && read_len == 9);
+
+    uint8_t buf2[2];
+    hex_to_buffer("f889", 4, &buf2, 2);
+    read_len = get_decode_length(buf2, 200, &decoded_len, &type);
+    assert(decoded_len == 2 && type == LIST && read_len == 137);
+
+    hex_to_buffer("b911", 4, &buf2, 2);
+    read_len = get_decode_length(buf2, 20, &decoded_len, &type);
+    assert(decoded_len == 2 && type == STRING && read_len == 17);
+}
+
+/**
+ * 期待的递归栈数据：次数,item_num,decoded_len,s tart_ptr,range
+ *                  1   0x89     2           seq + 2   0x89 - 2
+ *                  2   1        2           seq + 2   0x89 - 2
+ *                  1   0x89     2           seq + 2   0x89 - 2
+ *                  1   0x89     2           seq + 2   0x89 - 2
+ * 
+ */
 int rlp_decode(decode_result *my_result, uint8_t *seq, int seq_len)
 {
     if (seq_len == 0)
         return 0;
     seq_type type = NONE;                                                // 保存此次将要处理的数据的类型
-    uint8_t decoded_len;                                                 // 保存此次解码过的长度
+    int decoded_len;                                                     // 保存此次解码过的长度
     int item_num = get_decode_length(seq, seq_len, &decoded_len, &type); // 保存需要解码的长度
     uint8_t *start_ptr = seq + decoded_len;                              // 解码的起始地址
-
+    int need_decode_len = seq_len - decoded_len;
     if (type == STRING)
     {
         if (my_result->capacity < my_result->used_index)
@@ -79,14 +117,14 @@ int rlp_decode(decode_result *my_result, uint8_t *seq, int seq_len)
             // TODO:申请更大的内存空间，并拷贝旧数据，释放旧空间
         }
         // TODO: used_index越界了，理想情况是不应该越界的
-        my_result->data[my_result->used_index] = malloc(sizeof(uint8_t) * item_num); 
+        my_result->data[my_result->used_index] = malloc(sizeof(uint8_t) * item_num);
         memcpy(my_result->data[my_result->used_index], start_ptr, item_num);
         my_result->used_index++;
-        rlp_decode(my_result, start_ptr, item_num);
+        rlp_decode(my_result, start_ptr, need_decode_len);
     }
     else if (type == LIST)
     {
-        rlp_decode(my_result, start_ptr, item_num);
+        rlp_decode(my_result, start_ptr, need_decode_len);
     }
 }
 
@@ -176,7 +214,8 @@ int hex_to_buffer(const uint8_t *hex, size_t hex_len, uint8_t *out, size_t out_l
 
 int main(int argc, uint8_t const *argv[])
 {
-    uint8_t *seq = "f889008609184e72a00082271094000000000000000000000000000000000000000000a47f74657374320000000000000000000000000000000000000000000000000000006000571ca05e1d3a76fbf824220eafc8c79ad578ad2b67d01b0c2425eb1f1347e8f50882aba05bd428537f05f9830e93792f90ea6a3e2d1ee84952dd96edbae9f658f831ab13";
+    test_get_decode_length();
+    uint8_t seq[] = "f889008609184e72a00082271094000000000000000000000000000000000000000000a47f74657374320000000000000000000000000000000000000000000000000000006000571ca05e1d3a76fbf824220eafc8c79ad578ad2b67d01b0c2425eb1f1347e8f50882aba05bd428537f05f9830e93792f90ea6a3e2d1ee84952dd96edbae9f658f831ab13";
     uint8_t buffer[(sizeof(seq)) / 2] = {0};
     hex_to_buffer(seq, sizeof(seq) - 1, buffer, (sizeof(seq) - 1) / 2);
 
