487f603caba459f305c1cdaaaa8360f4e11539ad
wangkaixuan <2654189525@qq.com>
2019-09-04 13:15:58 +0800

    ensure our encode rule is rlp

--- rlp.c
+++ rlp.c
@@ -223,7 +223,8 @@ int buffer_to_hex(const uint8_t *buffer, size_t buffer_len, uint8_t *out, size_t
     return i * 2;
 }
 
-void test_rlp_decode(){
+void test_rlp_decode()
+{
     uint8_t seq[] = "f889008609184e72a00082271094000000000000000000000000000000000000000000a47f74657374320000000000000000000000000000000000000000000000000000006000571ca05e1d3a76fbf824220eafc8c79ad578ad2b67d01b0c2425eb1f1347e8f50882aba05bd428537f05f9830e93792f90ea6a3e2d1ee84952dd96edbae9f658f831ab13";
     uint8_t buffer[(sizeof(seq)) / 2] = {0};
     hex_to_buffer(seq, sizeof(seq) - 1, buffer, (sizeof(seq) - 1) / 2);
@@ -236,19 +237,62 @@ void test_rlp_decode(){
 
     rlp_decode(&my_resut, buffer, sizeof(buffer) / sizeof(buffer[0]));
 
-    assert(strcmp(my_resut.data[0],"00") == 0);
-    assert(strcmp(my_resut.data[1],"09184e72a000") == 0);
-    assert(strcmp(my_resut.data[2],"2710") == 0);
-    assert(strcmp(my_resut.data[3],"0000000000000000000000000000000000000000") == 0);
-    assert(strcmp(my_resut.data[4],"00") == 0);
-    assert(strcmp(my_resut.data[5],"7f7465737432000000000000000000000000000000000000000000000000000000600057") == 0);
-    assert(strcmp(my_resut.data[6],"1c") == 0);
-    assert(strcmp(my_resut.data[7],"5e1d3a76fbf824220eafc8c79ad578ad2b67d01b0c2425eb1f1347e8f50882ab") == 0);
-    assert(strcmp(my_resut.data[8],"5bd428537f05f9830e93792f90ea6a3e2d1ee84952dd96edbae9f658f831ab13") == 0);
+    assert(strcmp(my_resut.data[0], "00") == 0);
+    assert(strcmp(my_resut.data[1], "09184e72a000") == 0);
+    assert(strcmp(my_resut.data[2], "2710") == 0);
+    assert(strcmp(my_resut.data[3], "0000000000000000000000000000000000000000") == 0);
+    assert(strcmp(my_resut.data[4], "00") == 0);
+    assert(strcmp(my_resut.data[5], "7f7465737432000000000000000000000000000000000000000000000000000000600057") == 0);
+    assert(strcmp(my_resut.data[6], "1c") == 0);
+    assert(strcmp(my_resut.data[7], "5e1d3a76fbf824220eafc8c79ad578ad2b67d01b0c2425eb1f1347e8f50882ab") == 0);
+    assert(strcmp(my_resut.data[8], "5bd428537f05f9830e93792f90ea6a3e2d1ee84952dd96edbae9f658f831ab13") == 0);
 }
+
+void test_rlp_decode1()
+{
+    uint8_t seq[] = "f8aa018504e3b2920083030d409486fa049857e0209aa7d9e616f7eb3b3b78ecfdb080b844a9059cbb0000000000000000000000001febdb3539341a3005f3c5851854db7720a037fe00000000000000000000000000000000000000000000000002c68af0bb1400001ba0fe13bc4be8dc46e804f5b7eb6182d1f6feecdc2574eafface0ecc7f7be3516f4a042586cf962a3896e607214eede1a5cde727e13b62f86678efb980d9047a1017e";
+    uint8_t buffer[(sizeof(seq)) / 2] = {0};
+    hex_to_buffer(seq, sizeof(seq) - 1, buffer, (sizeof(seq) - 1) / 2);
+
+    decode_result my_resut;
+    char no_use;
+    my_resut.data = malloc(sizeof(&no_use) * DECODE_RESULT_LEN);
+    my_resut.capacity = DECODE_RESULT_LEN;
+    my_resut.used_index = 0;
+
+    rlp_decode(&my_resut, buffer, sizeof(buffer) / sizeof(buffer[0]));
+
+    for (size_t i = 0; i < my_resut.used_index; i++)
+    {
+        printf("index:%d,data:%s\n", i, my_resut.data[i]);
+    }
+}
+
+void test_rlp_decode2()
+{
+    uint8_t seq[] = "f8a903850a2fa8e8e2825dc09486fa049857e0209aa7d9e616f7eb3b3b78ecfdb080b844a9059cbb0000000000000000000000001febdb3539341a3005f3c5851854db7720a037fe000000000000000000000000000000000000000000000000016345785d8a00001ca023a0f5e57bfa3b518919078e1aa98c854a641322b1e2c7c6272c27d81e380d99a00c6102a9c0114772f9c64cc9c7aeccb7296640db079e35781276c66a0927b7ba";
+    uint8_t buffer[(sizeof(seq)) / 2] = {0};
+    hex_to_buffer(seq, sizeof(seq) - 1, buffer, (sizeof(seq) - 1) / 2);
+
+    decode_result my_resut;
+    char no_use;
+    my_resut.data = malloc(sizeof(&no_use) * DECODE_RESULT_LEN);
+    my_resut.capacity = DECODE_RESULT_LEN;
+    my_resut.used_index = 0;
+
+    rlp_decode(&my_resut, buffer, sizeof(buffer) / sizeof(buffer[0]));
+
+    for (size_t i = 0; i < my_resut.used_index; i++)
+    {
+        printf("index:%d,data:%s\n", i, my_resut.data[i]);
+    }
+}
+
 int main(int argc, uint8_t const *argv[])
 {
     test_get_decode_length();
     test_rlp_decode();
+    test_rlp_decode1();
+    test_rlp_decode2();
     return 0;
 }
